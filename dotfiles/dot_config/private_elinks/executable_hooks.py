0acc38c5ef5a7f6ee88d37df152059ff226d65dc
Adam M. Dickison <adam.dickison@posteo.net>
2025-08-08 15:31:44 -0400

    reset elinks config and add aliases

--- dotfiles/dot_config/private_elinks/executable_hooks.py
+++
@@ -1,9 +0,0 @@
-import elinks
-import hooks
-import devtools
-
-def pre_format_html_hook(url, html):
-    reload(devtools)
-    html = devtools.modifier(url, html)
-    html = devtools.replacer(url, html)
-    return html
